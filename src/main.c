8bf7b8c2aa308ce354bc2379a824d63624f50a97
Brayden Rudisill <braydenrudisill@gmail.com>
2025-05-15 11:57:23 -0700

    P3

--- src/main.c
+++ src/main.c
@@ -11,256 +11,134 @@
 #include "utils.h"
 #include "lcd.h"
 
-const unsigned short OverflowsPerTick = XTAL_FRQ / 256 / 256;
-volatile unsigned short OverflowCount = 0;
-volatile unsigned short EditIndex = 0;
-volatile bool IsHoldingButton = false;
-volatile unsigned char UnconfirmedEdits[14] = {
-    0,0,0,0,  // Year
-    0,0,      // Month
-    0,0,      // Day
-    0,0,      // Hour
-    0,0,      // Minute
-    0,0       // Second
-};
+enum States { Playing, Paused } State;
+bool IsPressed;
+
+typedef enum {
+    A, As, B, C, Cs, D, Ds, E, F, Fs, G, Gs
+} Note;
+
+typedef enum {
+    Whole, Half, Quarter, Eighth
+} Duration;
 
 typedef struct {
-    volatile int year;
-    volatile unsigned char month;
-    volatile unsigned char day;
-    volatile unsigned char hour;
-    volatile unsigned char minute;
-    volatile unsigned char second;
-} DateTime;
+    Note note;
+    Duration duration;
+} PlayingNote;
 
-volatile DateTime* DT;
+PlayingNote song[] = {
+    {E, Quarter},    // Mary
+    {D, Quarter},    // had
+    {C, Quarter},    // a
+    {D, Quarter},    // lit-
+    {E, Quarter},    // tle
+    {E, Quarter},    // lamb
+    {E, Half},       // (hold)
 
-void InitDT() {
-    DT = (DateTime*) malloc(sizeof(DateTime));
-    DT->year = 2025;
-    DT->month = 0;
-    DT->day = 0;
-    DT->hour = 0;
-    DT->minute = 0;
-    DT->second = 0;
-}
+    {D, Quarter},    // lit-
+    {D, Quarter},    // tle
+    {D, Half},       // lamb
 
-char* Months[12] = {
-    "Jan",
-    "Feb",
-    "Mar",
-    "Apr",
-    "May",
-    "Jun",
-    "Jul",
-    "Aug",
-    "Sep",
-    "Oct",
-    "Nov",
-    "Dec"
+    {E, Quarter},    // lit-
+    {G, Quarter},    // tle
+    {G, Half},       // lamb
+
+    {E, Quarter},    // Mary
+    {D, Quarter},    // had
+    {C, Quarter},    // a
+    {D, Quarter},    // lit-
+    {E, Quarter},    // tle
+    {E, Quarter},    // lamb
+    {E, Half},       // (hold)
+
+    {E, Quarter},    // Its
+    {D, Quarter},    // fleece
+    {D, Quarter},    // was
+    {E, Quarter},    // white
+    {D, Quarter},    // as
+    {C, Whole}       // snow
 };
 
-unsigned char DaysInMonths[12] = {
-    31,
-    28,
-    31,
-    30,
-    31,
-    30,
-    31,
-    31,
-    30,
-    31,
-    30,
-    31
+
+int lengths[] = {
+    400,
+    200,
+    100,
+    50,
 };
 
-bool year_div(const unsigned short n) {
-    return DT->year % n == 0;
-}
+int wait_times[] = {
+    142,
+    134,
+    127,
+    120,
+    113,
+    106,
+    100,
+    95,
+    90,
+    85,
+    80,
+    75,
+};
 
-bool IsLeapYear() {
-    return (year_div(4) && !year_div(100)) || year_div(400);
-}
+unsigned long CurrentNote;
 
-void TimerSet() {
-    TCCR0 = 4;   // Disable prescaling
-    TCNT0 = 0;   // Set initial count to 0
-    TIMSK |= 1;  // Timer0 overflow interrupt enable
-    sei();       // Global interrupt enable
-}
-
-enum OP_States { OP_Edit, OP_Display } OP_State = OP_Display;
-enum CT_States { CT_AMPM, CT_24hr } CT_State = CT_24hr;
-
-void TickDT() {
-    if (OP_State != OP_Display)
-        return;
-    if (DT==NULL)
+void PlayNextNote() {
+    if (CurrentNote >= sizeof(song) / sizeof(song[0]))
         return;
 
-    ++DT->second;
-    if (DT->second >= 60) {
-        DT->second = 0;
-        ++DT->minute;
-    }
-    if (DT->minute >= 60) {
-        DT->minute = 0;
-        ++DT->hour;
-    }
-    if (DT->hour >= 24) {
-        DT->hour = 0;
-        ++DT->day;
-    }
-    if ((IsLeapYear() && DT->month == 1 && DT->day >= 29) || ((!IsLeapYear() || DT->month != 1) && DT->day >= DaysInMonths[DT->month])) {
-        DT->day = 0;
-        ++DT->month;
-    }
-    if (DT->month >= 12) {
-        DT->month = 0;
-        ++DT->year;
-    }
-}
+    PlayingNote note = song[CurrentNote++];
 
-ISR(TIMER0_OVF_vect) {
-    if (++OverflowCount >= OverflowsPerTick) {
-        TickDT();
-        OverflowCount = 0;
-    }
-}
-
-
-// PC is [r1, r2, r3, r4, c1, c2, c3, c4]
-bool IsPressed(const unsigned char row, const unsigned char column) {
-    const unsigned char column_port = column + 4;
-    DDRC = 0x00;
-    PORTC = 0x00;
-
-    SET_BIT(DDRC, row);
-    CLR_BIT(PORTC, row);
-    // 000(0)_000(1)
-
-    CLR_BIT(DDRC, column_port);
-    SET_BIT(PORTC, column_port);
-
-    for (int i = 0; i < 2000; ++i)
-        NOP();
-
-    // 000(1)_000(0)
-    return GET_BIT(PINC, column_port) == 0;
-}
-
-void UpdateClockType() {
-    switch(CT_State) {
-        case CT_AMPM:
-            if (IsPressed(0, 3))
-                 CT_State = CT_24hr;
-            break;
-        case CT_24hr:
-            if (IsPressed(1, 3))
-                CT_State = CT_AMPM;
-            break;
-    }
-}
-
-void UpdateOperationMode() {
-    switch(OP_State) {
-        case OP_Display:
-            if (IsPressed(2, 3)) {
-                OP_State = OP_Edit;
-                lcd_clr();
-                EditIndex = 0;
-            }
-        break;
-        case OP_Edit:
-            if (IsPressed(3, 3) && EditIndex==14) {
-                OP_State = OP_Display;
-                DT->month = 10 * UnconfirmedEdits[0] + UnconfirmedEdits[1] - 1;
-                DT->day = 10 * UnconfirmedEdits[2] + UnconfirmedEdits[3] - 1;
-                DT->year = 1000 * UnconfirmedEdits[4] + 100 * UnconfirmedEdits[5] + 10 * UnconfirmedEdits[6] + UnconfirmedEdits[7];
-                DT->hour = 10 * UnconfirmedEdits[8] + UnconfirmedEdits[9];
-                DT->minute = 10 * UnconfirmedEdits[10] + UnconfirmedEdits[11];
-                DT->second = 10 * UnconfirmedEdits[12] + UnconfirmedEdits[13];
-            }
-        break;
-    }
-}
-int GetNumberPressed() {
-    int r, c;
-    if (IsPressed(3, 1))
-        return 0;
-
-    for (r = 0; r < 3; ++r)
-        for (c = 0; c < 3; ++c)
-            if (IsPressed(r, c))
-                return 3 * r + c + 1;
-
-    return -1;
-}
-void HandleEdits() {
-    SET_BIT(DDRB, 4);
-    if (EditIndex % 2 == 0)
-        SET_BIT(PORTB, 4);
-    else
-        CLR_BIT(PORTB, 4);
-
-    int n = GetNumberPressed();
-
-    if (n == -1) {
-        IsHoldingButton = false;
-        return;
-    }
-
-    if(!IsHoldingButton && EditIndex < 14) {
-        UnconfirmedEdits[EditIndex++] = n;
-        IsHoldingButton = true;
-        lcd_pos(0,0);
-        for (int i = 0; i < EditIndex; ++i) {
-            char buf[1];
-            sprintf(buf, "%1d", UnconfirmedEdits[i]);
-            lcd_puts2(buf);
-        }
-    }
-
-}
-
-void UpdateDisplay() {
-    char buf[17];
-
-    // Print date on top row.
-    lcd_pos(0, 0);
-    sprintf(buf, "%02d/%02d/%04d      ", DT->month + 1, DT->day + 1, DT->year);
-    lcd_puts2(buf);
-
-    // Do similar thing to print time on bottom row.
-    if (CT_State == CT_24hr) {
-        lcd_pos(1, 0);
-        sprintf(buf, "%02d:%02d:%02d      ", DT->hour, DT->minute, DT->second);
-        lcd_puts2(buf);
-    }
-    else {
-        lcd_pos(1, 0);
-        sprintf(buf, "%02d:%02d:%02d %s", DT->hour == 0 ? 12 : ((char) DT->hour - 1) % 12 + 1, DT->minute, DT->second, DT->hour < 12 ? "AM" : "PM");
-        lcd_puts2(buf);
+    for (int i = 0; i < lengths[note.duration] * 3; ++i) {
+        SET_BIT(PORTC, 0);
+        avr_wait(wait_times[note.note]);
+        CLR_BIT(PORTC, 0);
+        avr_wait(wait_times[note.note]);
     }
 }
 
 void Update() {
     wdt_reset();
-    UpdateOperationMode();
-    UpdateClockType();
-    if (OP_State == OP_Edit)
-        HandleEdits();
-    else if (OP_State == OP_Display)
-        UpdateDisplay();
+    SET_BIT(DDRC, 0);
+    CLR_BIT(DDRC, 1);
+    SET_BIT(DDRC, 5);
+
+    if (State == Playing) {
+        CLR_BIT(PORTC, 5);
+        if (!IsPressed && GET_BIT(PINC, 1)) {
+            State = Paused;
+            IsPressed = true;
+        }
+        else
+            PlayNextNote();
+    }
+    if (State == Paused) {
+        SET_BIT(PORTC, 5);
+        if (!IsPressed && GET_BIT(PINC, 1)) {
+            CurrentNote = 0;
+            State = Playing;
+            IsPressed = true;
+        }
+    }
+    if (IsPressed && !GET_BIT(PINC, 1))
+        IsPressed = false;
 }
 
 int main(void) {
+    State = Paused;
+    IsPressed = false;
+    CurrentNote = 0;
     avr_init();
     lcd_init();
-    InitDT();
-    TimerSet();
-
+    lcd_clr();
+    lcd_pos(0,0);
+    lcd_puts2("Marry had a litt");
+    lcd_pos(1,0);
+    lcd_puts2("le lamb!");
+    // InitDT();
+    // TimerSet();
     while(1) Update();
     return 0;
 }
--- src/utils.c
+++ src/utils.c
@@ -11,12 +11,11 @@ void avr_init()
 	WDTCR = 15;
 }
 
-void avr_wait(unsigned short msec) {
+void avr_wait(unsigned short usec_8s) {
 	TCCR0 = 3;
-	while (msec--) {
-		TCNT0 = (unsigned char)(256 - (XTAL_FRQ / 64) * 0.001);
-		SET_BIT(TIFR, TOV0);
-		while (!GET_BIT(TIFR, TOV0));
-	}
+
+	TCNT0 = (unsigned char)(256 - (XTAL_FRQ / 64) * 0.000008 * usec_8s);
+	SET_BIT(TIFR, TOV0);
+	while (!GET_BIT(TIFR, TOV0));
 	TCCR0 = 0;
 }
