6d0609e7073baa3b958b274aca8dd3032b3fe48b
Brayden Rudisill <braydenrudisill@gmail.com>
2025-05-29 11:23:17 -0700

    working ADC omg yippee

--- src/main.c
+++ src/main.c
@@ -102,23 +102,31 @@ void PlayNextNote() {
 void Update() {
 }
 
-int get_sample() {
-    ADMUX = 0b01000000;
-    ADCSRA = 0b11000000;
-    while (GET_BIT(ADCSRA, 6));
+
+void ADC_Init() {
+    DDRA = 0x00;
+    ADCSRA = 0x87;
+    ADMUX = 0x40;
+}
+
+int ADC_Read(char channel) {
+    ADMUX = ADMUX | (channel & 0x0f);
+    SET_BIT(ADCSRA, ADSC);
+    while (GET_BIT(ADCSRA, 4));
     int adcl = ADCL;
     int adch = ADCH;
 
-    return adcl | (adch << 8);
+    return adcl  + (adch * 256);
 }
 
 int main(void) {
     char buf[20];
     avr_init();
+    ADC_Init();
     lcd_init();
     while(1) {
         wdt_reset();
-        sprintf(buf, "%d        ", get_sample());
+        sprintf(buf, "%d        ", ADC_Read(0));
         lcd_pos(0, 0);
         lcd_puts2(buf);
         avr_wait(500);
